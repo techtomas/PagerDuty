2294368a32339e6eb5b93529d3b7987877aa4d83
Thomas Anderson <me@tjanderson.net>
2013-11-19 17:02:01 -0800

    Initial import of project
    Basic design and db use created to interact with the API and show this as a potentially viable solution

---
+++ models/Incident.js
@@ -0,0 +1,30 @@
+var Mongoose = require('mongoose');
+
+exports.IncidentSchema = new Mongoose.Schema({
+  id: String,
+  incident_number: Number,
+  created_on: Date,
+  status: String,
+  html_url: String,
+  incident_key: String,
+  service: {
+    id: String,
+    name: String,
+    html_url: String,
+    deleted_at: String
+  },
+  escaltion_policy: {
+    id: String,
+    name: String
+  },
+  assigned_to_user: String,
+  trigger_summary_data: {
+    subject: String
+  },
+  trigger_details_html_url: String,
+  trigger_type: String,
+  last_status_change_on: Date,
+  last_status_change_by: String,
+  number_of_escalations: Number,
+  resolved_by_user: String
+});
